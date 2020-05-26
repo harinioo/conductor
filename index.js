bb2af28eb0e6cae11f61dc2b07c746c765fa9196
harinioo <64526959+harinioo@users.noreply.github.com>
2020-05-26 15:17:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+
+
+var model = undefined;
+const classifierElement = document.getElementById('classifier');
+const loaderElement = document.getElementById('loader');
+
+async function initialize() {
+
+    model = await tf.loadLayersModel('model/model.json');
+    classifierElement.style.display = 'block';
+    loaderElement.style.display = 'none';
+
+    document.getElementById('predict').addEventListener('click', () => predict());
+
+}
+
+async function predict () {
+
+alert("submitted");
+    const imageElement = document.getElementById('img');
+    let tensorImg = tf.browser.fromPixels(imageElement).resizeNearestNeighbor([150, 150]).toFloat().expandDims();
+    prediction = await model.predict(tensorImg).data();
+
+    if (prediction[0] == 0) {
+
+        alert("You uploaded a nonmask!");
+
+    } else if (prediction[0] == 1) {
+
+        alert("You uploaded a mask!");
+
+    } else {
+        alert("Hummm... a weird error occurred.");
+    }
+
+}
+
+function changeImage() {
+    var imageDisplay = document.getElementById('img');
+    var uploadedImage = document.getElementById('my-file-selector').files[0];
+    imageDisplay.src = URL.createObjectURL(uploadedImage);
+}
+
+initialize();
